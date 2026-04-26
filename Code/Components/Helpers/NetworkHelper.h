cd422d17f69c8d59e3fb5da7c9880b9db6fc1096
RandomPerson189 <32617562+randomperson189@users.noreply.github.com>
2026-04-27 02:10:03 +0800

    Add NetworkHelper

---
+++ Code/Components/Helpers/NetworkHelper.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <CryCore/StaticInstanceList.h>
+#include <CryEntitySystem/IEntitySystem.h>
+#include <CrySchematyc/Utils/SharedString.h>
+
+////////////////////////////////////////////////////////
+// Physicalized bullet shot from weaponry, expires on collision with another object
+////////////////////////////////////////////////////////
+class CNetworkHelperComponent final : public IEntityComponent
+{
+public:
+	CNetworkHelperComponent();
+	virtual ~CNetworkHelperComponent();
+
+	// IEntityComponent
+	virtual void Initialize() override;
+
+	static void ReflectType(Schematyc::CTypeDesc<CNetworkHelperComponent>& desc);
+	// ~IEntityComponent
+
+	bool IsServer();
+};
