9e3a6572bb9f09148eb1ccdb49b5bcd3f630543f
RandomPerson189 <32617562+randomperson189@users.noreply.github.com>
2026-09-10 19:52:24 +0800

    Revert "Reworked CharacterControllerHelper"
    This reverts commit 4f7b6ccfb0c9548b239c31914173d49e163ac15d.

--- Code/Components/Helpers/CharacterControllerHelper.h
+++ Code/Components/Helpers/CharacterControllerHelper.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <DefaultComponents/Physics/CharacterControllerComponent.h>
-
 #include <CryCore/StaticInstanceList.h>
 #include <CryEntitySystem/IEntitySystem.h>
 #include <CrySchematyc/Utils/SharedString.h>
@@ -29,13 +27,9 @@ public:
 	}
 	// ~IEntityComponent
 
-	void SetMovementParams(const Cry::DefaultComponents::CCharacterControllerComponent::SMovement movement);
-	void ExpandMovementParams(const Cry::DefaultComponents::CCharacterControllerComponent::SMovement& movement, float& airControl, float& airResistance, float& inertia, float& inertiaAcceleration, float& maxClimbAngle, float& maxJumpAngle, float& minSlideAngle, float& minFallAngle, float& maxGroundVelocity);
-	Cry::DefaultComponents::CCharacterControllerComponent::SMovement CreateMovementParams(float airControl, float airResistance, float inertia, float inertiaAcceleration, float maxClimbAngle, float maxJumpAngle, float minSlideAngle, float minFallAngle, float maxGroundVelocity);
+	float GetDimensions(/*float& radius, */float& height, float& zOffset, bool& bCapsule, float& groundContactEps);
+	void SetDimensions(/*float mass, */float radius, float height, float zOffset, bool bCapsule, float groundContactEps);
 
-	void SetPhysicsParams(const Cry::DefaultComponents::CCharacterControllerComponent::SPhysics physics, float zOffset);
-	void ExpandPhysicsParams(const Cry::DefaultComponents::CCharacterControllerComponent::SPhysics& physics, float& radius, float& height, bool& capsule, float& groundContactEps, bool& sendCollisionSignal);
-	Cry::DefaultComponents::CCharacterControllerComponent::SPhysics CreatePhysicsParams(float radius, float height, bool capsule, float groundContactEps, bool sendCollisionSignal);
-
-	float GetZOffset();
+	/*CryTransform::CTransform GetTransform();
+	void SetTransform(CryTransform::CTransform transform);*/
 };
--- Code/Schematyc/CustomSchematycFunctions.cpp
+++ Code/Schematyc/CustomSchematycFunctions.cpp
@@ -77,92 +77,20 @@ namespace Schematyc
 			return false;
 		}
 
-		bool CylinderCast(const Vec3& origin, const Vec3& direction, const float& radius, const float& height, Vec3& hitPt, Vec3& hitNormal, const ExplicitEntityId& ignoredEntity, ExplicitEntityId& hitEntityId)
-		{
-			IPhysicalEntity* pPhysEnt = nullptr;
-
-			if (IEntity* pIgnoredEntity = gEnv->pEntitySystem->GetEntity((EntityId)ignoredEntity))
-			{
-				pPhysEnt = pIgnoredEntity->GetPhysics();
-			}
-
-			// Create the cylinder
-			primitives::cylinder cylinder;
-			cylinder.center = origin;
-			cylinder.axis = Vec3(0, 0, 1);
-			cylinder.r = radius;
-			cylinder.hh = height / 2;
-
-			IPhysicalWorld::SPWIParams pwiparams;
-
-			geom_contact* pContacts = nullptr;
-
-			intersection_params intersectionParams;
-			intersectionParams.bSweepTest = false;
-
-			pwiparams.pprim = &cylinder;
-			pwiparams.itype = cylinder.type;
-			pwiparams.sweepDir = direction;
-			pwiparams.entTypes = ent_all;
-
-			pwiparams.pSkipEnts = &pPhysEnt;
-			pwiparams.nSkipEnts = 1;
-
-			pwiparams.ppcontact = &pContacts;
-			pwiparams.pip = &intersectionParams;
-
-			if (gEnv->pPhysicalWorld->PrimitiveWorldIntersection(pwiparams))
-			{
-				// First contact
-				hitPt = pContacts[0].pt;
-				hitNormal = pContacts[0].n;
-
-				const int physicsEntityId = pContacts[0].iPrim[0];
-
-				if (IPhysicalEntity* pHitPhysEnt = gEnv->pPhysicalWorld->GetPhysicalEntityById(physicsEntityId))
-				{
-					if (IEntity* pEntity = gEnv->pEntitySystem->GetEntityFromPhysics(pHitPhysEnt))
-					{
-						hitEntityId = ExplicitEntityId(pEntity->GetId());
-					}
-				}
-
-				return true;
-			}
-
-			return false;
-		}
-
 		static void RegisterFunctions(IEnvRegistrar& registrar)
 		{
 			CEnvRegistrationScope scope = registrar.Scope(GetTypeDesc<ray_hit>().GetGUID());
 			{
-				{
-					auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&RayCastAdvanced, "{11C536CD-C889-43CB-BC26-47CA5D41809D}"_cry_guid, "RayCastAdvanced");
-					pFunction->BindInput(1, 'orig', "Origin", "The source position to cast from");
-					pFunction->BindInput(2, 'dir', "Direction", "The direction and magnitude to cast to");
-					pFunction->BindInput(5, 'iid', "Ignored Entity Id", "The id of the entity that we ignore");
-					pFunction->BindOutput(0, 'hit', "Hit", "Whether or not the ray hit");
-					pFunction->BindOutput(3, 'pt', "Position", "The position that we hit");
-					pFunction->BindOutput(4, 'norm', "Normal", "The normal of the object that we hit");
-					pFunction->BindOutput(6, 'eid', "Entity Id", "The id of the entity that we hit");
-					pFunction->SetDescription("Casts a ray through the world's physical grid");
-					scope.Register(pFunction);
-				}
-				{
-					auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CylinderCast, "{D79DD7AA-56C5-4BD7-9854-21E4D5E2F230}"_cry_guid, "CylinderCast");
-					pFunction->BindInput(1, 'orig', "Origin", "The source position to cast from");
-					pFunction->BindInput(2, 'dir', "Direction", "The direction and magnitude to cast to");
-					pFunction->BindInput(3, 'rad', "Radius", "Radius of the cylinder");
-					pFunction->BindInput(4, 'hgt', "Height", "Height of the cylinder");
-					pFunction->BindInput(7, 'iid', "Ignored Entity Id", "The id of the entity that we ignore");
-					pFunction->BindOutput(0, 'hit', "Hit", "Whether or not the ray hit");
-					pFunction->BindOutput(5, 'pt', "Position", "The position that we hit");
-					pFunction->BindOutput(6, 'norm', "Normal", "The normal of the object that we hit");
-					pFunction->BindOutput(8, 'eid', "Entity Id", "The id of the entity that we hit");
-					pFunction->SetDescription("Casts a cylinder through the world's physical grid");
-					scope.Register(pFunction);
-				}
+				auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&RayCastAdvanced, "{11C536CD-C889-43CB-BC26-47CA5D41809D}"_cry_guid, "RayCastAdvanced");
+				pFunction->BindInput(1, 'orig', "Origin", "The source position to cast from");
+				pFunction->BindInput(2, 'dir', "Direction", "The direction and magnitude to cast to");
+				pFunction->BindInput(5, 'iid', "Ignored Entity Id", "The id of the entity that we ignore");
+				pFunction->BindOutput(0, 'hit', "Hit", "Whether or not the ray hit");
+				pFunction->BindOutput(3, 'pt', "Position", "The position that we hit");
+				pFunction->BindOutput(4, 'norm', "Normal", "The normal of the object that we hit");
+				pFunction->BindOutput(6, 'eid', "Entity Id", "The id of the entity that we hit");
+				pFunction->SetDescription("Casts a ray through the world's physical grid");
+				scope.Register(pFunction);
 			}
 		}
 	}
